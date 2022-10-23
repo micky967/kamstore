a6239710c1373d56f2e5c207ab9d21743af855cc
manjit <you@example.com>
2022-10-23 11:11:34 -0700

    google auth

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6eXlgvb9QcMTVwPdX-tjZcV-INty2WsI",
+  authDomain: "kamstore-2051c.firebaseapp.com",
+  projectId: "kamstore-2051c",
+  storageBucket: "kamstore-2051c.appspot.com",
+  messagingSenderId: "1072670449937",
+  appId: "1:1072670449937:web:d3d70243f555a62620ecb0"
+};
